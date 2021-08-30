2f59a3ff660e0778a097e84d57f4bb823c20e2de
João Pedro <joaopleao99@gmail.com>
2021-08-29 23:08:25 -0300

    Process Mapper

---
+++ process_mapper.js
@@ -0,0 +1,319 @@
+define (["jquery", "fab/list-plugin"], function (jQuery, FbListPlugin) {
+    var FbListProcess_mapper = new Class ({
+        Extends:FbListPlugin,
+        initialize: function (options) {
+            this.parent(options);
+        },
+        buttonAction: function () {
+            var id_selected = [], input_clicked, xml_bpmn, body;
+            this.listform.getElements('input[name^=ids]').each(function (id) {
+                if (id.get('value') !== false && id.checked !== false) {
+                    id_selected.push(id.get('value'));
+                }
+            });
+
+            if (id_selected.length === 1) {
+                if (jQuery("#processMapperModal").length) {
+                    jQuery("#processMapperModal").remove();
+                }
+
+                this.setProcessElements(id_selected[0]);
+            }
+        },
+        createModal: function() {
+            var modal = document.createElement('div');
+            modal.setAttribute('class', 'process_mapper-modal');
+            modal.setAttribute('id', 'processMapperModal');
+
+            var modalContent = document.createElement('div');
+            modalContent.setAttribute('class', 'process_mapper-modal-content');
+
+            var modalHeader = document.createElement('div');
+            modalHeader.setAttribute('class', 'process_mapper-modal-header');
+            var buttonClose = document.createElement('span');
+            buttonClose.setAttribute('class', 'process_mapper-modal-close');
+            buttonClose.innerHTML = '&times;';
+            var title = document.createElement('h2');
+            title.innerHTML = 'Mapeamento';
+            modalHeader.appendChild(buttonClose);
+            modalHeader.appendChild(title);
+
+            var modalBody = document.createElement('div');
+            modalBody.setAttribute('class', 'process_mapper-modal-body');
+
+            var table_mapper = document.createElement('div');
+            table_mapper.setAttribute('id', 'div_table_mapper');
+            table_mapper.setAttribute('class', 'table-responsive');
+            table_mapper.setAttribute('style', 'overflow: auto');
+
+            modalBody.appendChild(table_mapper);
+
+            /*var frameBPMN = document.createElement('iframe');
+            frameBPMN.setAttribute('src', Fabrik.liveSite + 'plugins/fabrik_list/process_modeler/modeler/public/index.html');
+            frameBPMN.setAttribute('style', 'width: 100%; height: 1500px');
+            modalBody.appendChild(frameBPMN);*/
+
+            var modalFooter = document.createElement('div');
+            modalFooter.setAttribute('class', 'process_mapper-modal-footer');
+
+            var validateButton = document.createElement('button');
+            validateButton.setAttribute('class', 'btn btn-default');
+            validateButton.setAttribute('id', 'validateButton');
+            validateButton.setAttribute('style', 'margin: 10px')
+            validateButton.innerHTML = "Validar";
+
+            var mapearTudoButton = document.createElement('button');
+            mapearTudoButton.setAttribute('class', 'btn btn-default');
+            mapearTudoButton.setAttribute('id', 'mapearTudo');
+            mapearTudoButton.setAttribute('style', '');
+            mapearTudoButton.innerHTML = "Mapear Tudo";
+
+            modalFooter.appendChild(validateButton);
+            modalFooter.appendChild(mapearTudoButton);
+
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(modalBody);
+            modalContent.appendChild(modalFooter);
+            modal.appendChild(modalContent);
+
+            var body = document.getElement("body");
+            var head = document.getElement('head');
+            var linkCss = document.createElement('link');
+            linkCss.setAttribute('href', this.options.url + 'plugins/fabrik_list/process_mapper/modal/modal.css');
+            linkCss.setAttribute('rel', 'stylesheet');
+
+            body.appendChild(modal);
+            head.appendChild(linkCss);
+        },
+        openModal: function () {
+            var modal = document.getElementById("processMapperModal");
+            var body = document.getElement("body");
+
+            body.style.overflowY = "hidden";
+            modal.style.display = "block";
+
+            var span = document.getElementsByClassName("process_mapper-modal-close")[0];
+
+            span.onclick = function() {
+                modal.style.display = "none";
+                body.style.overflowY = "auto";
+            };
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                    body.style.overflowY = "auto";
+                }
+            };
+        },
+        setProcessElements: function (processId) {
+            var self = this;
+            self.options.processId = processId;
+
+            jQuery.ajax ({
+                url: Fabrik.liveSite + 'index.php',
+                method: "POST",
+                data: {
+                    'option': 'com_fabrik',
+                    'format': 'raw',
+                    'task': 'plugin.pluginAjax',
+                    'plugin': 'process_mapper',
+                    'method': 'elementsToMapper',
+                    'g': 'list',
+                    'processId': processId,
+                    'table': self.options.table,
+                    'elementBpmn': self.options.elementBpmn,
+                    'infos': self.options.infos
+                }
+            }).done (function (data) {
+                self.options.processElements = JSON.parse(data);
+
+                self.createModal();
+                self.createTable();
+                self.validateButton();
+                self.mapearTudo();
+                self.openModal();
+            });
+        },
+        createTable: function () {
+            var div = document.getElementById('div_table_mapper');
+            var table = document.createElement('table');
+            table.setAttribute('class', 'table');
+            table.setAttribute('id', 'table_mapper')
+            var thead = document.createElement('thead');
+            var tr_head = document.createElement('tr');
+            var first_th = document.createElement('th');
+            first_th.setAttribute('scope', 'col');
+            first_th.innerHTML = 'Elemento do Diagrama';
+            var second_th = document.createElement('th');
+            second_th.setAttribute('scope', 'col');
+            second_th.innerHTML = 'Tipo';
+            var third_th = document.createElement('th');
+            third_th.setAttribute('scope', 'col');
+            third_th.innerHTML = 'Estado';
+            var fourth_th = document.createElement('th');
+            fourth_th.setAttribute('scope', 'col');
+            fourth_th.innerHTML = 'Observações';
+
+            tr_head.appendChild(first_th);
+            tr_head.appendChild(second_th);
+            tr_head.appendChild(third_th);
+            tr_head.appendChild(fourth_th);
+            thead.appendChild(tr_head);
+            table.appendChild(thead);
+
+            var tbody = document.createElement('tbody');
+            var i, tr, td1, td2, td3, td4, a_link;
+            var rows = this.options.processElements;
+            var self = this;
+            for (i=0; i<rows.length; i++) {
+                tr = document.createElement('tr');
+                td1 = document.createElement('td');
+                td1.innerHTML = rows[i].titulo;
+                td2 = document.createElement('td');
+                td2.innerHTML = rows[i].tipo;
+                td3 = document.createElement('td');
+                a_link = document.createElement('a');
+                if (rows[i].delete_el) {
+                    a_link.setAttribute('href', '#');
+                    a_link.setAttribute('id', i);
+                    a_link.innerHTML = "Excluir";
+                    a_link.onclick = function () {
+                        jQuery.ajax ({
+                            url: Fabrik.liveSite + 'index.php',
+                            method: "POST",
+                            data: {
+                                'option': 'com_fabrik',
+                                'format': 'raw',
+                                'task': 'plugin.pluginAjax',
+                                'plugin': 'process_mapper',
+                                'method': 'deleteElement',
+                                'g': 'list',
+                                'elementRow': self.options.processElements[this.getAttribute('id')],
+                                'infos': self.options.infos
+                            }
+                        }).done ((data) => {
+                            var parent_td = this.getParent();
+                            var parent_row = parent_td.getParent();
+                            parent_row.remove();
+                        });
+                    }
+                }
+                else if (rows[i].mapped) {
+                    a_link.setAttribute('href', "javascript:window.open('" + Fabrik.liveSite + "index.php?option=com_fabrik&view=form&formid=" + this.options.infos.form_id + "&rowid=" + rows[i].id + "','_blank')");
+                    a_link.setAttribute('target', '');
+                    a_link.setAttribute('formvalue', this.options.infos.form_id);
+                    a_link.innerHTML = 'Atualizar';
+                }
+                else {
+                    a_link.setAttribute('href', '#');
+                    a_link.setAttribute('id', i);
+                    a_link.innerHTML = 'Mapear';
+                    a_link.onclick = function() {
+                        jQuery.ajax ({
+                            url: Fabrik.liveSite + 'index.php',
+                            method: "POST",
+                            data: {
+                                'option': 'com_fabrik',
+                                'format': 'raw',
+                                'task': 'plugin.pluginAjax',
+                                'plugin': 'process_mapper',
+                                'method': 'createElement',
+                                'g': 'list',
+                                'elementRow': self.options.processElements[this.getAttribute('id')],
+                                'infos': self.options.infos
+                            }
+                        }).done ((data) => {
+                            this.setAttribute('href', "javascript:window.open('" + Fabrik.liveSite + "index.php?option=com_fabrik&view=form&formid=" + self.options.infos.form_id + "&rowid=" + data + "','_blank')");
+                            this.setAttribute('target', '');
+                            this.setAttribute('formvalue', self.options.infos.form_id);
+                            this.innerHTML = "Atualizar";
+                            this.onclick = function () {};
+                            self.options.processElements[this.getAttribute('id')].mapped = true;
+                        });
+                    }
+                }
+                td3.appendChild(a_link);
+                td4 = document.createElement('td');
+                td4.innerHTML = rows[i].notes;
+
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                tr.appendChild(td4);
+                tbody.appendChild(tr);
+            }
+
+            table.appendChild(tbody);
+            div.appendChild(table);
+        },
+        validateButton: function () {
+            var button = document.getElementById('validateButton');
+
+            var self = this;
+            button.onclick = function () {
+                if (self.options.processElements.length > 0) {
+                    var i;
+                    var rows = self.options.processElements;
+                    var mapped = true;
+                    var userTask = false;
+                    for (i=0; i<rows.length; i++) {
+                        if (rows[i].mapped === false) {
+                            mapped = false;
+                        }
+                        if (rows[i].tipo === 'userTask') {
+                            userTask = true;
+                        }
+                    }
+                    if (!mapped) {
+                        alert("Faltam elementos para mapear!");
+                    }
+                    else if (!userTask) {
+                        alert("Necessário pelo menos uma tarefa de usuário!");
+                    }
+                    else {
+                        jQuery.ajax ({
+                            url: Fabrik.liveSite + 'index.php',
+                            method: "POST",
+                            data: {
+                                'option': 'com_fabrik',
+                                'format': 'raw',
+                                'task': 'plugin.pluginAjax',
+                                'plugin': 'process_mapper',
+                                'method': 'updateProcessStatus',
+                                'g': 'list',
+                                'processId': self.options.processId,
+                                'table': self.options.table,
+                                'elementStatus': self.options.elementStatus,
+                                'statusValue': self.options.statusValue
+                            }
+                        }).done (function (data) {
+                            alert("Status do processo atualizado!");
+                        });
+                    }
+                }
+                else {
+                    alert("Não existem elementos no diagrama BPMN!!");
+                }
+            }
+        },
+        mapearTudo: function () {
+            var rows = this.options.processElements;
+            var button = document.getElementById('mapearTudo');
+
+            button.onclick = function () {
+                var i, a;
+                for (i=0; i<rows.length; i++) {
+                    a = document.getElementById(i);
+                    if (a) {
+                        if (a.innerHTML === 'Mapear') {
+                            a.click();
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    return FbListProcess_mapper;
+});
---
+++ process_mapper.php
@@ -0,0 +1,243 @@
+<?php
+
+defined('_JEXEC') or die('Restricted access');
+
+use Fabrik\Helpers\Image;
+
+require_once COM_FABRIK_FRONTEND . '/models/plugin-list.php';
+
+class PlgFabrik_ListProcess_mapper extends PlgFabrik_List
+{
+
+
+    public function button(&$args)
+    {
+        parent::button($args);
+
+        return true;
+    }
+
+    protected function getImageName()
+    {
+        return 'list';
+    }
+
+    protected function buttonLabel()
+    {
+        $params = $this->getParams();
+
+        return $params->get('pmapper_button_label', 'Mapear Processo');
+    }
+
+    protected function getAclParam()
+    {
+        return 'pmapper_access';
+    }
+
+    public function onElementsToMapper()
+    {
+        $infos = $_POST['infos'];
+        $processId = $_POST['processId'];
+        $table = $_POST['table'];
+        $elementBpmn = $_POST['elementBpmn'];
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select($elementBpmn)->from($table)->where('id = ' . (int)$processId);
+        $db->setQuery($query);
+        $xml = $db->loadResult();
+
+        $dom = new DOMDocument();
+        $dom->loadXML($xml);
+        $process = $dom->getElementsByTagName('process');
+        $types = array('startEvent', 'sequenceFlow', 'endEvent');
+        $els = array();
+        $bpmn_ids = array();
+
+        for ($j=0; $j<$process->length; $j++) {
+            $elements = $process->item($j)->childNodes;
+            $etapaAnterior = '';
+            for ($i = 0; $i < $elements->length; $i++) {
+                $element = $elements->item($i);
+                if ($element->nodeType === 1) {
+                    if (!in_array($element->localName, $types)) {
+                        $el = new stdClass();
+                        $el->titulo = $element->getAttribute('name');
+                        $el->tipo = $element->localName;
+                        $el->bpmn_id = $element->getAttribute('id');
+                        $el->processo = $processId;
+                        $el->xml = $dom->saveXML($element);
+                        $el->delete = false;
+                        $el->mapped = false;
+                        $el->notes = '';
+                        $el->etapaAnterior = $etapaAnterior;
+                        $etapaAnterior = $el->bpmn_id;
+
+                        $query = $db->getQuery(true);
+                        $query->select("id, {$infos['titulo']}, {$infos['tipo']}, {$infos['xml']}")->from($infos['table'])->where("{$infos['bpmn_id']} = '{$el->bpmn_id}' AND {$infos['processo']} = " . (int) $el->processo);
+                        $db->setQuery($query);
+                        $result = $db->loadAssoc();
+                        if ($result) {
+                            $el->mapped = true;
+                            $el->id = $result['id'];
+                            if ($el->xml !== $result[$infos['xml']]) {
+                                $el->notes .= "Desatualizado<br>";
+                            }
+                            if ($el->titulo !== $result['titulo']) {
+                                $el->notes .= "- Elemento '{$infos['titulo']}' foi alterado no diagrama.<br>";
+                            }
+                            if ($el->tipo !== $result['tipo']) {
+                                $el->notes .= "- Elemento '{$infos['tipo']}' foi alterado no diagrama.<br>";
+                            }
+                        }
+
+                        $els[] = $el;
+                        $bpmn_ids[] = $el->bpmn_id;
+                    }
+                }
+            }
+        }
+        $query = $db->getQuery(true);
+        $query->select("id, {$infos['titulo']}, {$infos['tipo']}, {$infos['bpmn_id']}")->from($infos['table'])->where("{$infos['processo']} = " . (int) $processId);
+        $db->setQuery($query);
+        $result = $db->loadAssocList();
+
+
+        foreach ($result as $item) {
+            if (!in_array($item[$infos['bpmn_id']], $bpmn_ids)) {
+                $el = new stdClass();
+                $el->id = $item['id'];
+                $el->titulo = $item[$infos['titulo']];
+                $el->tipo = $item[$infos['tipo']];
+                $el->bpmn_id = $item[$infos['bpmn_id']];
+                $el->processo = $processId;
+                $el->delete_el = true;
+                $el->notes = '- O elemento não existe no diagrama.<br>';
+                $els[] = $el;
+            }
+        }
+
+        echo json_encode($els);
+    }
+
+    public function onCreateElement() {
+        $row = $_POST['elementRow'];
+        $infos = $_POST['infos'];
+
+        $db = JFactory::getDbo();
+        $insert = array();
+        $insert['id'] = 0;
+        $insert[$infos['titulo']] = $row['titulo'];
+        $insert[$infos['bpmn_id']] = $row['bpmn_id'];
+        $insert[$infos['processo']] = $row['processo'];
+        $insert[$infos['tipo']] = $row['tipo'];
+        $insert[$infos['xml']] = $row['xml'];
+
+        if ($row['etapaAnterior'] !== '') {
+            $query = $db->getQuery(true);
+            $query->select('id')->from($infos['table'])->where("{$infos['bpmn_id']} = '{$row['etapaAnterior']}'");
+            $db->setQuery($query);
+            $etapaAnterior = $db->loadResult();
+            if ($etapaAnterior) {
+                $insert[$infos['etapaAnterior']] = $etapaAnterior;
+            }
+        }
+
+        $insert = (Object) $insert;
+
+        $db->insertObject($infos['table'], $insert, 'id');
+
+        echo $db->insertid();
+    }
+
+    public function onDeleteElement() {
+        $row = $_POST['elementRow'];
+        $infos = $_POST['infos'];
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->delete($infos['table'])->where('id = ' . (int) $row['id']);
+        $db->setQuery($query);
+        $db->execute();
+    }
+
+    public function onUpdateProcessStatus() {
+        $elementStatus = $_POST['elementStatus'];
+        $statusValue = $_POST['statusValue'];
+        $table = $_POST['table'];
+        $processId = $_POST['processId'];
+
+        $db = JFactory::getDbo();
+        $obj = new stdClass();
+        $obj->id = $processId;
+        $obj->$elementStatus = $statusValue;
+
+        $update = $db->updateObject($table, $obj, 'id');
+
+        echo $update;
+    }
+
+    protected function getInfos() {
+        $params = $this->getParams();
+        $info = new stdClass();
+
+        $elementList = $params->get('pme_table');
+        if ($elementList) {
+            $db = JFactory::getDbo();
+            $query = $db->getQuery(true);
+            $query->select('db_table_name, form_id')->from("#__fabrik_lists")->where('id = ' . (int)$elementList);
+            $db->setQuery($query);
+            $result = $db->loadAssoc();
+            $info->table = $result['db_table_name'];
+            $info->form_id = $result['form_id'];
+
+            if ($params->get('pme_titulo')) {
+                $info->titulo = str_replace("{$info->table}___", '', $params->get('pme_titulo'));
+            }
+            if ($params->get('pme_bpmn_id')) {
+                $info->bpmn_id = str_replace("{$info->table}___", '', $params->get('pme_bpmn_id'));
+            }
+            if ($params->get('pme_process')) {
+                $info->processo = str_replace("{$info->table}___", '', $params->get('pme_process'));
+            }
+            if ($params->get('pme_tipo')) {
+                $info->tipo = str_replace("{$info->table}___", '', $params->get('pme_tipo'));
+            }
+            if ($params->get('pme_etapa_anterior')) {
+                $info->etapaAnterior = str_replace("{$info->table}___", '', $params->get('pme_etapa_anterior'));
+            }
+            if ($params->get('pme_bpmn_xml')) {
+                $info->xml = str_replace("{$info->table}___", '', $params->get('pme_bpmn_xml'));
+            }
+        }
+
+        return $info;
+    }
+
+    public function onLoadJavascriptInstance($args)
+    {
+        parent::onLoadJavascriptInstance($args);
+
+        $listModel = $this->getModel();
+        $worker = FabrikWorker::getPluginManager();
+        $params = $this->getParams();
+
+        $opts             = $this->getElementJSOptions();
+        $opts->url = COM_FABRIK_LIVESITE;
+        $opts->infos = $this->getInfos();
+        $opts->table = $listModel->getTable()->db_table_name;
+        $opts->elementBpmn = $worker->getElementPlugin($params->get('pmapper_bpmn'))->element->name;
+        $opts->elementStatus = $worker->getElementPlugin($params->get('pmapper_status'))->element->name;
+        $opts->statusValue = $params->get('pmapper_status_update');
+
+        $opts             = json_encode($opts);
+        $this->jsInstance = "new FbListProcess_mapper($opts)";
+
+        return true;
+    }
+
+    public function loadJavascriptClassName_result()
+    {
+        return 'FbListProcess_mapper';
+    }
+}
